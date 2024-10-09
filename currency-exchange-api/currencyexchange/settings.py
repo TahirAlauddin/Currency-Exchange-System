1770555fa1ce812145549ae2eb7e7d703f58e146
tahir <tahiralauddin7@gmail.com>
2024-10-09 21:15:54 +0500

    feat: added cors headers
    localhost port 8000 and 3000 allowed in cors

--- currency-exchange-api/currencyexchange/settings.py
+++ currency-exchange-api/currencyexchange/settings.py
@@ -29,11 +29,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "corsheaders",
+
+
     'rest_framework',
     'transactions',
 ]
 
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://localhost:8000']
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
